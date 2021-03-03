3152bc495057b60283c1aaf94d53049d6a739126
Gorion <nev3rkill@protonmail.ch>
2021-03-03 13:24:39 +0100

    d20 icons

---
+++ module/config.js
@@ -0,0 +1,10 @@
+export const EXTINCION = {}
+
+EXTINCION.abilities = {
+  "str": "EXTINCION.AbilityStr",
+  "dex": "EXTINCION.AbilityDex",
+  "con": "EXTINCION.AbilityCon",
+  "int": "EXTINCION.AbilityInt",
+  "ner": "EXTINCION.AbilityNer",
+  "cha": "EXTINCION.AbilityCha"
+}
