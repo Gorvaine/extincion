ca9fca5ac3965cb32f80af629d52f8dc07ace8e2
NeV3rKilL <nev3rkill@gmail.com>
2021-12-25 15:52:00 +0100

    v9 Changes

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -58,7 +58,7 @@ export class extincionActorSheet extends ActorSheet {
     // Delete Inventory Item
     html.find('.item-delete').click(ev => {
       const li = $(ev.currentTarget).parents(".item");
-      this.actor.deleteOwnedItem(li.data("itemId"));
+      this.actor.deleteEmbeddedDocuments("Item",[li.data("itemId")]);
       li.slideUp(200, () => this.render(false));
     });
 
@@ -100,7 +100,7 @@ export class extincionActorSheet extends ActorSheet {
     delete itemData.data["type"];
 
     // Finally, create the item!
-    return this.actor.createOwnedItem(itemData);
+    return this.actor.createEmbeddedDocuments("Item" ,[itemData]);
   }
 
   /**
